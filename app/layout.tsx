9c74a369ea2fd9d1e4684d38f75ec5d876c29be6
Mohanad Hani <mohanadhani@Mohanads-MacBook-Air.local>
2024-03-11 21:18:40 +0300

    feat: Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,33 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/app/components/Navbar/Navbar";
+import {Analytics} from '@vercel/analytics/react';
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const inter = Inter({subsets: ["latin"], variable: "--font-inter"});
 const newake = localFont({
-  src: "../public/fonts/Newake-Font-Demo.otf",
-  variable: "--font-newake",
+    src: "../public/fonts/Newake-Font-Demo.otf",
+    variable: "--font-newake",
 });
 
 export const metadata: Metadata = {
-  title: "Mohanad Hani",
-  description: "Mohanad Hani's Portfolio Website",
+    title: "Mohanad Hani",
+    description: "Mohanad Hani's Portfolio Website",
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={`${inter.variable} ${newake.variable}`}>
-      <body>
-        <Navbar />
+    return (
+        <html lang="en" className={`${inter.variable} ${newake.variable}`}>
+        <body>
+        <Navbar/>
         {children}
-      </body>
-    </html>
-  );
+        <Analytics/>
+        </body>
+        </html>
+    );
 }
